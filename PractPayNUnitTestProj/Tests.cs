214a4c84eaebe73abe35ef012cd942b443e241f1
jreyvill <johnrey.villademosa@studytafensw.edu.au>
2022-11-21 10:34:15 +1100

    update

---
+++ PractPayNUnitTestProj/Tests.cs
@@ -0,0 +1,19 @@
+//Add the following reference
+using PracticePayProjectSANT;
+
+namespace PractPayNUnitTestProj
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
